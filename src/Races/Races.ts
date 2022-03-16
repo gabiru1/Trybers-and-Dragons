98c53b58239e43ff2d5df233514db3454a37952f
Gabriel Sampaio Bicalho <gabrielsampaiobicalho@gmail.com>
2022-03-16 16:57:46 -0300

    implementa classe Race

---
+++ src/Races/Races.ts
@@ -0,0 +1,22 @@
+abstract class Race {
+  constructor(
+    private readonly _name: string,
+    private readonly _dexterity: number,
+  ) {}
+
+  get name(): string {
+    return this._name;
+  }
+
+  get dexterity(): number {
+    return this._dexterity;
+  }
+
+  static createdRacesInstances(): number {
+    throw new Error('Not implemented');
+  }
+
+  abstract get maxLifePoints(): number;
+}
+
+export default Race;
