cf45bbcdbe927e82eee4ee2f99d37c10ed02c5ed
Gabriel Sampaio Bicalho <gabrielsampaiobicalho@gmail.com>
2022-03-17 16:44:18 -0300

    adiciona metodo estatico de archetype

--- src/Archetypes/Archetype.ts
+++ src/Archetypes/Archetype.ts
@@ -4,6 +4,10 @@ abstract class Archetype {
     private readonly _special: number = 0,
     private readonly _cost: number = 0,
   ) {}
+
+  static createdArchetypeInstances(): number {
+    throw new Error('Not implemented');
+  }
 }
 
 export default Archetype;
