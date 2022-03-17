fd9ca7c209a547cd693e07b2a0792e3433c6fc0e
Gabriel Sampaio Bicalho <gabrielsampaiobicalho@gmail.com>
2022-03-17 17:21:27 -0300

    importa e exporta no index

--- src/Archetypes/index.ts
+++ src/Archetypes/index.ts
@@ -1,6 +1,9 @@
 import Archetype from './Archetype';
 import Necromancer from './Necromancer';
+import Mage from './Mage';
+import Warrior from './Warrior';
+import Ranger from './Ranger';
 
-export { Necromancer };
+export { Necromancer, Mage, Warrior, Ranger };
 
 export default Archetype;
