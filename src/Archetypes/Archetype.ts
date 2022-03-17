ee9f99dd2a9773fc8f2411e6a28a9ac85243c7d7
Gabriel Sampaio Bicalho <gabrielsampaiobicalho@gmail.com>
2022-03-17 16:47:18 -0300

    implementa getters

--- src/Archetypes/Archetype.ts
+++ src/Archetypes/Archetype.ts
@@ -5,9 +5,23 @@ abstract class Archetype {
     private readonly _cost: number = 0,
   ) {}
 
+  get name(): string {
+    return this._name;
+  }
+
+  get special(): number {
+    return this._special;
+  }
+
+  get cost(): number {
+    return this._cost;
+  }
+
   static createdArchetypeInstances(): number {
     throw new Error('Not implemented');
   }
+
+  abstract get energyType(): 'mana' | 'stamina';
 }
 
 export default Archetype;
